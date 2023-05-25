aedc0cde507263e957be289db50990e71d6b0435
Pavel Shramov <psha@nguni.psha.org.ru>
2023-05-25 14:18:58 +0300

    cppbson: Header based BSON encoder

---
+++ src/tll/bson/cppbson.h
@@ -0,0 +1,195 @@
+// SPDX-License-Identifier: MIT
+
+#ifndef _TLL_UTIL_BSON_CPPBSON_H
+#define _TLL_UTIL_BSON_CPPBSON_H
+
+namespace tll::bson::cppbson {
+
+struct Memory
+{
+	const void * data = nullptr;
+	size_t size = 0;
+};
+
+enum class Type : uint8_t
+{
+	Double = 0x01,
+	UTF8 = 0x02,
+	Document = 0x03,
+	Array = 0x04,
+	Binary = 0x05,
+	Int32 = 0x10,
+	Int64 = 0x12,
+	Decimal128 = 0x13,
+};
+
+using Decimal128 = std::array<uint8_t, 16>;
+
+template <typename T>
+struct data_type {};
+
+template <> struct data_type<int32_t> { static constexpr auto value = Type::Int32; };
+template <> struct data_type<int64_t> { static constexpr auto value = Type::Int64; };
+template <> struct data_type<double> { static constexpr auto value = Type::Double; };
+
+template <typename T>
+static constexpr auto data_type_v = data_type<T>::value;
+
+template <typename View>
+struct Document
+{
+	View view;
+	unsigned offset = 4;
+
+	Document(View v) : view(v) {}
+
+	void ensure_size(size_t size)
+	{
+		if (view.size() < offset + size)
+			view.resize(offset + size);
+	}
+
+	void append_raw(const void * data, size_t size)
+	{
+		ensure_size(size);
+		append_raw_nocheck(data, size);
+	}
+
+	void append_raw_nocheck(const void * data, size_t size)
+	{
+		memcpy(view.view(offset).data(), data, size);
+		offset += size;
+	}
+
+	void append_key(Type type, std::string_view key)
+	{
+		ensure_size(1 + key.size() + 1);
+		return append_key_nocheck(type, key);
+	}
+
+	void append_key_nocheck(Type type, std::string_view key)
+	{
+		auto v = view.view(offset);
+		*v.template dataT<Type>() = type;
+		memcpy(v.view(1).data(), key.data(), key.size());
+		*v.view(1 + key.size()).template dataT<char>() = 0;
+		offset += 1 + key.size() + 1;
+	}
+
+	void append(Type type, std::string_view key, const void * data, size_t size)
+	{
+		ensure_size(1 + key.size() + 1 + size);
+		append_nocheck(type, key, data, size);
+	}
+
+	void append_nocheck(Type type, std::string_view key, const void * data, size_t size)
+	{
+		append_key_nocheck(type, key);
+		append_raw_nocheck(data, size);
+	}
+
+	template <typename T>
+	void append_scalar(std::string_view key, T value)
+	{
+		ensure_size(1 + key.size() + 1 + sizeof(value));
+		append_scalar_nocheck<T>(key, value);
+	}
+
+	template <typename T>
+	void append_scalar_nocheck(std::string_view key, T value)
+	{
+		append_key(data_type_v<T>, key);
+		ensure_size(sizeof(value));
+		*view.view(offset).template dataT<T>() = value;
+		offset += sizeof(value);
+	}
+
+	void append_int32(std::string_view key, int32_t value) { append_scalar(key, value); }
+	void append_int64(std::string_view key, int64_t value) { append_scalar(key, value); }
+	void append_double(std::string_view key, double value) { append_scalar(key, value); }
+
+	void append_decimal128(std::string_view key, const Decimal128 * value)
+	{
+		append(Type::Decimal128, key, value, sizeof(*value));
+	}
+
+	void append_utf8(std::string_view key, std::string_view value)
+	{
+		ensure_size(1 + key.size() + 1 + 4 + value.size() + 1);
+		append_utf8_nocheck(key, value);
+	}
+
+	void append_utf8_nocheck(std::string_view key, std::string_view value)
+	{
+		append_key_nocheck(Type::UTF8, key);
+		*view.view(offset).template dataT<int32_t>() = value.size() + 1;
+		offset += 4;
+		append_raw_nocheck(value.data(), value.size());
+		*view.view(offset).template dataT<char>() = 0;
+		offset++;
+	}
+
+	void append_binary(std::string_view key, const Memory & value)
+	{
+		ensure_size(1 + key.size() + 1 + 4 + value.size);
+		append_binary_nocheck(key, value);
+	}
+
+	void append_binary_nocheck(std::string_view key, const Memory & value)
+	{
+		append_key_nocheck(Type::Binary, key);
+		*view.view(offset).template dataT<int32_t>() = value.size;
+		offset += 4;
+		*view.view(offset++).template dataT<char>() = 0;
+		append_raw_nocheck(value.data, value.size);
+	}
+
+	Document append_document(std::string_view key)
+	{
+		append_key(Type::Document, key);
+		return Document(view.view(offset));
+	}
+
+	Document append_array(std::string_view key)
+	{
+		append_key(Type::Array, key);
+		return Document(view.view(offset));
+	}
+
+	void finish_document(Document &child)
+	{
+		child.finish_standalone();
+		offset += child.offset;
+	}
+
+	void finish_standalone()
+	{
+		*view.view(offset).template dataT<int8_t>() = 0;
+		*view.template dataT<int32_t>() = ++offset;
+	}
+};
+
+struct ArrayIndex
+{
+	std::array<char, 16> idxbuf;
+	unsigned idx = 0;
+
+	template <typename I, typename Buf>
+	std::string_view uint_to_string(I v, Buf &buf)
+	{
+		auto end = ((char *) buf.data()) + buf.size();
+		auto ptr = end;
+		do {
+			I r = v % 10;
+			v /= 10;
+			*--ptr = '0' + r;
+		} while (v);
+		return std::string_view(ptr, end - ptr);
+	}
+
+	std::string_view next() { return uint_to_string(idx++, idxbuf); }
+};
+
+} // namespace tll::bson::cppbson
+
+#endif//_TLL_UTIL_BSON_CPPBSON_H
---
+++ src/tll/bson/encoder.h
@@ -0,0 +1,159 @@
+// SPDX-License-Identifier: MIT
+
+#ifndef _TLL_UTIL_BSON_ENCODER_H
+#define _TLL_UTIL_BSON_ENCODER_H
+
+#include <tll/scheme.h>
+#include <tll/util/memoryview.h>
+
+#include "tll/bson/cppbson.h"
+#include "tll/bson/error-stack.h"
+#include "tll/bson/util.h"
+
+namespace tll::bson::cppbson {
+
+struct Encoder : public ErrorStack
+{
+	std::vector<char> buffer;
+
+	void init()
+	{
+		buffer.resize(64 * 1024);
+	}
+
+	std::optional<tll::const_memory> encode(const util::Settings &settings, const tll::scheme::Message * message, const tll_msg_t * msg)
+	{
+		Document bson(tll::make_view(buffer));
+		if (settings.seq_key.size())
+			bson.append_int64(settings.seq_key, (int64_t) msg->seq);
+		if (settings.mode == util::Settings::Mode::Flat) {
+			bson.append_utf8(settings.type_key, message->name);
+			if (!encode(bson, message, tll::make_view(*msg)))
+				return std::nullopt;
+		} else {
+			auto child = bson.append_document(message->name);
+			if (!encode(child, message, tll::make_view(*msg)))
+				return std::nullopt;
+			bson.finish_document(child);
+		}
+		bson.finish_standalone();
+		return tll::const_memory { bson.view.data(), bson.offset };
+	}
+
+	template <typename View, typename Buf>
+	bool encode(Document<View> &bson, const tll::scheme::Message * message, const Buf & buf);
+
+	template <typename View, typename Buf>
+	bool encode(Document<View> &bson, const tll::scheme::Field * field, std::string_view key, const Buf & buf);
+
+	template <typename View, typename Buf>
+	bool encode_list(Document<View> &bson, const tll::scheme::Field * field, std::string_view key, size_t size, size_t entity, const Buf & buf);
+};
+
+template <typename View, typename Buf>
+bool Encoder::encode(Document<View> &bson, const tll::scheme::Message * message, const Buf & buf)
+{
+	for (auto f = message->fields; f; f = f->next) {
+		if (!encode(bson, f, f->name, buf.view(f->offset)))
+			return fail_field(false, f);
+	}
+	return true;
+}
+
+template <typename View, typename Buf>
+bool Encoder::encode(Document<View> &bson, const tll::scheme::Field * field, std::string_view key, const Buf & data)
+{
+	using Field = tll::scheme::Field;
+	switch (field->type) {
+	case Field::Int8:
+		bson.append_int32(key, *data.template dataT<int8_t>()); return true;
+	case Field::Int16:
+		bson.append_int32(key, *data.template dataT<int16_t>()); return true;
+	case Field::Int32:
+		bson.append_int32(key, *data.template dataT<int32_t>()); return true;
+	case Field::Int64:
+		bson.append_int64(key, *data.template dataT<int64_t>()); return true;
+	case Field::UInt8:
+		bson.append_int32(key, *data.template dataT<uint8_t>()); return true;
+	case Field::UInt16:
+		bson.append_int32(key, *data.template dataT<uint16_t>()); return true;
+	case Field::UInt32:
+		bson.append_int64(key, *data.template dataT<uint32_t>()); return true;
+	case Field::UInt64:
+		return fail(false, "uint64 fields are not supported");
+	case Field::Double:
+		bson.append_double(key, *data.template dataT<double>()); return true;
+	case Field::Decimal128:
+		bson.append_decimal128(key, data.template dataT<Decimal128>()); return true;
+
+	case Field::Bytes:
+		if (field->sub_type == Field::ByteString) {
+			auto ptr = data.template dataT<char>();
+			bson.append_utf8(key, std::string_view(ptr, strnlen(ptr, field->size)));
+		} else
+			bson.append_binary(key, Memory { data.template dataT<uint8_t>(), field->size });
+		return true;
+
+	case Field::Array: {
+		auto size = tll::scheme::read_size(field->count_ptr, data);
+		if (size < 0)
+			return fail(false, "Negative count: {}", size);
+
+		auto af = field->type_array;
+		return encode_list(bson, af, key, size, af->size, data.view(af->offset));
+	}
+	case Field::Pointer: {
+		auto ptr = tll::scheme::read_pointer(field, data);
+		if (!ptr)
+			return fail(false, "Invalid offset ptr version: {}", field->offset_ptr_version);
+		if (data.size() < ptr->offset)
+			return fail(false, "Offset pointer out of bounds: +{} < {}", ptr->offset, data.size());
+		if (field->sub_type == Field::ByteString) {
+			if (ptr->size == 0)
+				bson.append_utf8(key, "");
+			else
+				bson.append_utf8(key, std::string_view(data.view(ptr->offset).template dataT<const char>(), ptr->size - 1));
+			return true;
+		}
+		return encode_list(bson, field->type_ptr, key, ptr->size, ptr->entity, data.view(ptr->offset));
+	}
+	case Field::Message: {
+		auto child = bson.append_document(key);
+		if (!encode(child, field->type_msg, data))
+			return false;
+		bson.finish_document(child);
+		return true;
+	}
+	case Field::Union: {
+		auto type = tll::scheme::read_size(field->type_union->type_ptr, data.view(field->type_union->type_ptr->offset));
+		if (type < 0 || (size_t) type > field->type_union->fields_size)
+			return fail(false, "Union type out of bounds: {}", type);
+		auto uf = field->type_union->fields + type;
+
+		auto child = bson.append_document(key);
+		if (!encode(child, uf, uf->name, data.view(uf->offset)))
+			return fail_field(false, uf);
+		bson.finish_document(child);
+		return true;
+	}
+	}
+	return false;
+}
+
+template <typename View, typename Buf>
+bool Encoder::encode_list(Document<View> &bson, const tll::scheme::Field * field, std::string_view key, size_t size, size_t entity, const Buf & data)
+{
+	auto child = bson.append_array(key);
+
+	std::array<char, 10> idxbuf;
+	for (auto i = 0u; i < size; i++) {
+		if (!encode(child, field, util::uint_to_string(i, idxbuf), data.view(entity * i)))
+			return fail_index(false, i);
+	}
+	bson.finish_document(child);
+	return true;
+}
+
+} // namespace tll::bson::cppbson
+
+#endif//_TLL_UTIL_BSON_ENCODER_H
